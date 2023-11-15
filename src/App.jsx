7d6abd78a6a5287d19af42ab2565aa6c03dbee9b
devbella <bellachoi1210@gmail.com>
2023-11-15 10:06:08 +0900

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div className="bg-red-100">Chat GPT!</div>;
+  return (
+    <div className="min-h-screen max-w-screen-md mx-auto flex">
+      <div className="bg-blue-100 w-52">Side</div>
+      <div className="grow flex flex-col">
+        <div className="bg-yellow-100 grow">Main</div>
+        <div className="bg-purple-100 h-24">Chat Bar</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
